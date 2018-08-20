d9af58644bd110962b3c366ca42651c69e2b289b
LeThinh2511 <thinhlefrs@gmail.com>
2018-08-20 09:31:17 +0700

    using repository patten and rxswift

---
+++ MovieDB_IOS/MovieDB_IOS/Data/Source/Remote/API/Request/RequestMovieDB.swift
@@ -0,0 +1,79 @@
+//
+//  RequestMovieDB.swift
+//  MovieDB_IOS
+//
+//  Created by ThinhLe on 8/13/18.
+//  Copyright © 2018 ThinhLe. All rights reserved.
+//
+
+import Foundation
+
+class RequestMovieDB {
+    static let shared = RequestMovieDB()
+    private let apiKey = "a13cd0049bdd3d3275de11627248af15"
+    private let baseURL = "https://api.themoviedb.org/3/"
+
+    func buildRequest(field: FieldMovieDB, queryID: Int?, parameters: [String: Any]?) -> URLRequest? {
+        let path: String
+        let url: String
+        var queryItems = [URLQueryItem]()
+        let baseParams = ["api_key": self.apiKey, "language": "en-US"]
+        addingQueryItems(to: &queryItems, parameters: baseParams)
+        if let parameters = parameters {
+            addingQueryItems(to: &queryItems, parameters: parameters)
+        }
+        if let id = queryID {
+            path = insertID(toField: field.rawValue, identifier: id)
+            url = baseURL + path
+        } else {
+            url = baseURL + field.rawValue
+        }
+        var components = URLComponents(string: url)
+        components?.queryItems = queryItems
+        guard let urlComponents = components?.url
+        else {
+            return nil
+        }
+        return URLRequest(url: urlComponents)
+    }
+
+    // func to append fieldMovieDB when query has id
+    private func insertID(toField path: String, identifier: Int) -> String {
+        var editPath = path
+        let pathSeparator: Character = "/"
+        let tempIndex = editPath.index(of: pathSeparator)
+        if let index = tempIndex {
+            let indexToInsert = editPath.index(after: index)
+            if indexToInsert.encodedOffset == editPath.count {
+                editPath.append(String(identifier))
+            } else {
+                let idString = "\(identifier)/"
+                editPath.insert(contentsOf: idString, at: index)
+            }
+            return editPath
+        }
+        return editPath + String(pathSeparator) + String(identifier)
+    }
+
+    // func to append queryItems with parameters
+    private func addingQueryItems(to queryItems: inout [URLQueryItem], parameters: [String: Any]) {
+        for (key, value) in parameters {
+            let valueString = String(describing: value)
+            let queryItem = URLQueryItem(name: key, value: valueString)
+            queryItems.append(queryItem)
+        }
+    }
+}
+
+enum FieldMovieDB: String {
+    case searchPerson = "search/person"
+    case movieDetail = "movie/"
+    case movieCredits = "movie/credits"
+    case genreList = "genre/movie/list"
+    case personDetail = "person/"
+    case moviePopular = "movie/popular"
+    case movieNowPlaying = "movie/now_playing"
+    case movieUpcoming = "movie/upcoming"
+    case movieTopRate = "movie/top_rated"
+    case searchMovie = "search/movie"
+}
---
+++ MovieDB_IOS/MovieDB_IOS/Data/Source/Remote/Service/Service.swift
@@ -0,0 +1,63 @@
+//
+//  Service.swift
+//  MovieDB_IOS
+//
+//  Created by ThinhLe on 8/13/18.
+//  Copyright © 2018 ThinhLe. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import RxSwift
+class Service {
+
+    private var session: URLSession
+
+    static let shared: Service = {
+        let session = URLSession(configuration: .default)
+        let service = Service(session: session)
+        return service
+    }()
+
+    private init(session: URLSession) {
+        self.session = session
+    }
+
+    // Making request and return
+    func request<T: Mappable>(request: URLRequest?) -> Observable<T> {
+        return Observable.create { observer in
+            guard let requestMovieDB = request
+            else {
+                    observer.onError(RequestMovieDBError.buildRequestError)
+                observer.onCompleted()
+                return Disposables.create()
+            }
+            self.session.dataTask(with: requestMovieDB, completionHandler: { (data, response, _) in
+                if let response = response as? HTTPURLResponse {
+                    if response.statusCode == 200 {
+                        guard let data = data
+                        else {
+                            observer.onError(RequestMovieDBError.buildRequestError)
+                            observer.onCompleted()
+                            return
+                        }
+                        let json = String(data: data, encoding: .utf8)
+                        guard let jsonString = json, let object = T(JSONString: jsonString)
+                        else {
+                            observer.onError(RequestMovieDBError.buildRequestError)
+                            observer.onCompleted()
+                            return
+                        }
+                        observer.onNext(object)
+                        observer.onCompleted()
+                    }
+                }
+            }).resume()
+            return Disposables.create()
+        }
+    }
+}
+
+enum RequestMovieDBError: Error {
+    case buildRequestError
+}
